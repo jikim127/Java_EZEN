d507fae8e95aad39691b8eee26fb60773db69061
jikim127 <129915198+jikim127@users.noreply.github.com>
2023-05-15 23:24:26 +0900

    Add files via upload

---
+++ Java_Homework_01/src/ex01/Exam4x06.java
@@ -0,0 +1,16 @@
+package ex01;
+
+public class Exam4x06 {
+
+	public static void main(String[] args) {
+		
+
+		for(int i=1; i<=6; i++) {
+			for(int j=1; j<=6; j++) {
+				if(i + j == 6)
+					System.out.println(i +" + "+ j +" = 6");
+			}
+		}
+
+	}
+}

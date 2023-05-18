c78ef613adaaddbd84b36edbf64c62ae57edb009
jikim127 <129915198+jikim127@users.noreply.github.com>
2023-05-18 18:01:11 +0900

    Add files via upload

---
+++ Java_Homework_02/src/ex01/Exam5x01.java
@@ -0,0 +1,19 @@
+package ex01;
+
+public class Exam5x01 {
+
+	public static void main(String[] args) {
+
+		// 5-1
+		// 답: a,d,e,f 잘못됨.
+		/* 이유: a - 변수이름이 와야 하는데 [] 배열이와서.
+			    d - 배열을 잡아놓고 바로 뒤에 각각의 배열안을 선언했는데 그렇게는 못함.
+			    e - int [5] arr 가 되야함.
+			    f - int[][] arr = new int[3][2]가 되야함.
+		
+		*/
+		
+		
+	}
+
+}
